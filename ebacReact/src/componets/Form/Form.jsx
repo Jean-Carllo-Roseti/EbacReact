ec3697b021101cc23a5a165500c8f4ada57b1d14
jean carllo <jeanrosete04@gmail.com>
2024-01-16 20:03:06 -0300

    finalizado a logica, encontrando apenas uma dificuldade para a mensagem inicial da pagina.

--- ebacReact/src/componets/Form/Form.jsx
+++ ebacReact/src/componets/Form/Form.jsx
@@ -1,24 +1,63 @@
+import { useState } from "react";
+
 const Formulario = () => {
+let [peso, setPeso] = useState(0);
+let [altura, setAltura] = useState(0);
+
+const exibeIMC = () => {
+    if (peso === 0 && altura === 0) {
+        return <p className="fs-5 resultado">Insira os dados</p>;
+    }
+
+    const resultante = peso / (altura * altura);
+    const resultado = resultante.toFixed(1);
+    
+    if(resultado > 39.9){
+        return (
+            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 3 (mórbida) </p>
+            )
+    } else if (resultado >= 35.0){
+        return (
+            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 2 (severa)</p>
+            )
+    } else if (resultado >= 30.0){
+        return (
+            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 1</p>
+            )
+    } else if (resultado >= 25.0){
+        return (
+            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta levemente acima do peso</p>
+            )
+    } else if (resultado >= 186){
+        return (
+            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta no peso ideal </p>
+            )
+    } else {
+        return (
+            <p className="fs-5 resultado" >Seu IMC é de ({resultado}), Você esta a baixo do peso </p>
+            )
+    } 
+}
+
+
     return (
         <div className="container">
             <form className="form mt-4">
                 <div className="row justify-content-center">
                     <div className="col-2">
-                <input type="number" className="form-control " placeholder="Insira sua altura" />
+                <input type="number" className="form-control peso " placeholder="Insira sua peso"   onChange={evento => setPeso(evento.target.value)}/>
                     </div>
                     <div className="col-2">
-                <input type="number" className="form-control " placeholder="Insira seu peso" />
+                <input type="number" className="form-control altura" placeholder="Insira seu altura" onChange={evento => setAltura(evento.target.value)}  />
                     </div>
-                    <button type="button" className="btn btn-primary col-1">
-                        Calcular
-                    </button>
                 </div>
             </form>
             <div className="text-center p-5">
-                <p className="fs-5">Seu imc é de 25</p>
+                {exibeIMC()}
             </div>
         </div>
     )
 }
+
 
 export default Formulario;
