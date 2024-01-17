5d9b863ec7ebf35dd5d85b4ed993d53f822f86d0
jean carllo <jeanrosete04@gmail.com>
2024-01-17 13:01:39 -0300

    alterado o placeholder, adicionado peso e altura em seus respectivos campos.

--- ebacReact/src/componets/Form/Form.jsx
+++ ebacReact/src/componets/Form/Form.jsx
@@ -3,45 +3,45 @@ import { useState } from "react";
 
 
 const Formulario = () => {
-let [peso, setPeso] = useState(0);
-let [altura, setAltura] = useState(0);
+    let [peso, setPeso] = useState(0);
+    let [altura, setAltura] = useState(0);
 
 
-const exibeIMC = () => {
-    if (peso.toString().length < 2 || altura.toString().length < 2 || isNaN(peso) || isNaN(altura)) {
-        return <p className="fs-5 resultado">Insira os dados corretamente</p>;
+    const exibeIMC = () => {
+        if (peso.toString().length < 2 || altura.toString().length < 2 || isNaN(peso) || isNaN(altura)) {
+            return <p className="fs-5 resultado">Insira os dados corretamente</p>;
+        }
+
+
+        const resultante = peso / (altura * altura);
+        const resultado = resultante.toFixed(1);
+
+        if (resultado > 39.9) {
+            return (
+                <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 3 (mórbida) </p>
+            )
+        } else if (resultado >= 35.0) {
+            return (
+                <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 2 (severa)</p>
+            )
+        } else if (resultado >= 30.0) {
+            return (
+                <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 1</p>
+            )
+        } else if (resultado >= 25.0) {
+            return (
+                <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta levemente acima do peso</p>
+            )
+        } else if (resultado >= 18.6) {
+            return (
+                <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta no peso ideal </p>
+            )
+        } else {
+            return (
+                <p className="fs-5 resultado" >Seu IMC é de ({resultado}), Você esta a baixo do peso </p>
+            )
+        }
     }
-    
-
-    const resultante = peso / (altura * altura);
-    const resultado = resultante.toFixed(1);
-    
-    if(resultado > 39.9){
-        return (
-            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 3 (mórbida) </p>
-            )
-    } else if (resultado >= 35.0){
-        return (
-            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 2 (severa)</p>
-            )
-    } else if (resultado >= 30.0){
-        return (
-            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você detem o nivel de obesidade grau 1</p>
-            )
-    } else if (resultado >= 25.0){
-        return (
-            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta levemente acima do peso</p>
-            )
-    } else if (resultado >= 18.6){
-        return (
-            <p className="fs-5 resultado" > Seu IMC é de ({resultado}), Você esta no peso ideal </p>
-            )
-    } else {
-        return (
-            <p className="fs-5 resultado" >Seu IMC é de ({resultado}), Você esta a baixo do peso </p>
-            )
-    } 
-}
 
 
     return (
@@ -49,15 +49,15 @@ const exibeIMC = () => {
             <form className="form mt-4">
                 <div className="row justify-content-center">
                     <div className="col-2">
-                <input type="number" className="form-control peso "  mask='000 KG' maskPlaceholder="_"  onChange={evento => setPeso(evento.target.value)}/>
+                        <input type="number" className="form-control peso " mask='000 KG' placeholder="insira seu peso " onChange={evento => setPeso(evento.target.value)} />
                     </div>
                     <div className="col-2">
-                <input type="number" className="form-control altura" mask='0,00 CM'  maskPlaceholder="_"  onChange={evento => setAltura(evento.target.value)}  />
+                        <input type="number" className="form-control altura" mask='0,00 CM' placeholder="insira sua altura" onChange={evento => setAltura(evento.target.value)} />
                     </div>
                 </div>
             </form>
             <div className="text-center p-5">
-                {  exibeIMC()}
+                {exibeIMC()}
             </div>
         </div>
     )
